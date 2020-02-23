e6590523f877c41cfc3fdd1d5acb15bab5bead95
Neo Ahn <ahynest@naver.com>
2020-02-23 18:37:08 +0900

    Update common.sql

--- login/common.sql
+++ login/common.sql
@@ -13,12 +13,10 @@
 
 
 -- Dumping database structure for common
-DROP DATABASE IF EXISTS `common`;
 CREATE DATABASE IF NOT EXISTS `common` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `common`;
 
 -- Dumping structure for table common.logs
-DROP TABLE IF EXISTS `logs`;
 CREATE TABLE IF NOT EXISTS `logs` (
   `log_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `procedure_nm` varchar(200) NOT NULL COMMENT 'Function or Procedure name',
@@ -39,12 +37,11 @@ CREATE TABLE IF NOT EXISTS `logs` (
 /*!40000 ALTER TABLE `logs` ENABLE KEYS */;
 
 -- Dumping structure for table common.users
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `user_id` smallint(3) unsigned NOT NULL AUTO_INCREMENT COMMENT '사용자 id',
   `user_nm` varchar(20) NOT NULL COMMENT '엑셀의 사용자 이름으로 사용',
   `user_gb` varchar(5) NOT NULL DEFAULT 'WP' COMMENT 'SA, AM(리포트), MG(실무관리), WP(실무)',
-  `user_pw` varchar(128) NOT NULL DEFAULT '1' COMMENT '비밀번호',
+  `user_pw` varchar(128) DEFAULT NULL COMMENT '비밀번호',
   `pw_initialize` tinyint(1) unsigned NOT NULL DEFAULT 1 COMMENT '1: 최초접속(비밀번호 초기화)',
   `user_ip` varchar(20) DEFAULT NULL,
   `programv` varchar(20) NOT NULL DEFAULT 'programv' COMMENT '프로그램버전',
@@ -59,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   KEY `user_nm` (`user_nm`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='프로그램 사용자';
 
--- Dumping data for table common.users: ~0 rows (approximately)
+-- Dumping data for table common.users: ~1 rows (approximately)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
