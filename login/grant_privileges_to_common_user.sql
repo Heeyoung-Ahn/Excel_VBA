1d2537818c16c0701f135a5a76fff561ecba57c5
Neo Ahn <ahynest@naver.com>
2020-02-27 23:59:29 +0900

    delete login project

--- login/grant_privileges_to_common_user.sql
+++
@@ -1,9 +0,0 @@
--- common 사용자의 비밀번호 변경하여 실행
--- common 사용자에게 부여하는 권한
--- 1) common.users: SELECT, UPDATE
--- 2) common.logs: INSERT
-CREATE USER 'common'@'%' IDENTIFIED BY 'password';
-GRANT USAGE ON *.* TO 'common'@'%';
-GRANT INSERT  ON TABLE common.logs TO 'common'@'%';
-GRANT SELECT, UPDATE  ON TABLE common.users TO 'common'@'%';
-FLUSH PRIVILEGES;
